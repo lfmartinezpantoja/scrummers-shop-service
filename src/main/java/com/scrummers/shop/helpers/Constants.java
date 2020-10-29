9dc749db40b7868a17cbd235403e839ced2fee9f
lfmartinez <lfmartinezpantoja@gmail.com>
2020-10-29 18:02:27 -0500

    add sleuth for tracing

--- src/main/java/com/scrummers/shop/helpers/Constants.java
+++ src/main/java/com/scrummers/shop/helpers/Constants.java
@@ -3,4 +3,7 @@ package com.scrummers.shop.helpers;
 public class Constants {
 
 	public static final String SUCCESFULLY_REQUEST_LOG = "--- Peticion procesada con exito ---";
+
+	private Constants() {
+	}
 }
